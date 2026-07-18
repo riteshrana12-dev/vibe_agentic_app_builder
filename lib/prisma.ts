cf2f436641e49c0170f32600afbba58e6c129354
riteshrana12-dev <ranaritesh677@gmail.com>
2026-07-18 19:34:10 +0530

    fix prisma client import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,21 @@
 // lib/prisma.ts
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 function createPrismaClient() {
-  const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL!,
+  if (!process.env.DATABASE_URL) {
+    throw new Error(
+      "DATABASE_URL is not defined in your environment variables.",
+    );
+  }
+  const pool = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
   });
-  return new PrismaClient({ adapter });
+  return new PrismaClient({ adapter: pool });
 }
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
